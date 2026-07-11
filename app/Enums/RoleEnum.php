77487b8e6769c3a826f9bd7d19ae9bcb11093977
agaba3290 <agaba3290@gmail.com>
2026-07-11 15:08:25 +0300

    Add files via upload

--- app/Enums/RoleEnum.php
+++ app/Enums/RoleEnum.php
@@ -4,7 +4,7 @@ namespace App\Enums;
 
 enum RoleEnum: string
 {
-    case Student= 'Student';
-    case Lecturer= 'Lecturer';
-    case Admin= 'Admin';
+    case Student= 'student';
+    case Lecturer= 'lecturer';
+    case Admin= 'admin';
 }
