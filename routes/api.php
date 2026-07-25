13a08ef6f89e2a6e1b0e3aee72bb4894c06bb346
Noerine <noerinenamuganza3@gmail.com>
2026-07-25 12:01:23 +0300

    add API endpoints for browsing, joining, and leaving groups

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\DirectMessageController;
 use App\Http\Controllers\RecommendationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GroupController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -22,7 +23,12 @@ Route::post('/login', [AuthController::class, 'apiLogin']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/messages/send', [MessageController::class, 'send'])->middleware('not_blacklisted');
     Route::get('/messages/group/{groupId}', [MessageController::class, 'getMessages']);
-
+    Route::post('/quizzes',[\App\Http\Controllers\QuizController::class, 'apiStore']);
+   //Browse all groups (mine + joinable)
+   Route::get('/groups/browse', [GroupController::class, 'browse']);
+   Route::post('/groups/{id}/join', [GroupController::class, 'join']);
+   Route::post('/groups/{id}/leave', [GroupController::class, 'leave']);
+   
     // Groups (my groups)
     Route::get('/groups', function (Request $request) {
         return response()->json($request->user()->groups()->orderBy('name')->get());
@@ -34,9 +40,7 @@ Route::middleware('auth:sanctum')->group(function () {
     $quiz = \App\Models\Quiz::with('questions')->findOrFail($id);
     return response()->json($quiz->questions);
     });
-    Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/quizzes',[\App\Http\Controllers\QuizController::class, 'apiStore']);
-});
+        
     // Direct messages
     Route::post('/direct-messages/send', [DirectMessageController::class, 'send'])->middleware('not_blacklisted');
     Route::get('/direct-messages/{userId}', [DirectMessageController::class, 'getConversation']);
